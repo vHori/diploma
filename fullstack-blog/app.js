cbb5e96fa87b67ac58411f46712163daa87e1eb9
Віталік <vitalik@Vitaliks-MacBook-Pro.local>
2019-04-02 16:46:06 +0300

    Initial commit

---
+++ fullstack-blog/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const path = require('path')
+const postRouter = require('./routes/post.js')
+const keys = require('./keys')
+
+const port = process.env.PORT || 5000
+const clientPath = path.join(__dirname, 'client')
+
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true})
+	.then(() => console.log('MongoDB connected.'))
+	.catch(err => console.log(err))
+
+const app = express()
+app.use(bodyParser.json())
+app.use('/api/post', postRouter)
+app.use(express.static(clientPath))
+
+app.listen(port, () => {
+	console.log(`Server has been started on port ${port}`)
+})
---
+++ fullstack-blog/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+	mongoURI: 'mongodb+srv://kiraigg:m2006v2000@fullstack-blog-zk8ur.mongodb.net/test?retryWrites=true'
+}
